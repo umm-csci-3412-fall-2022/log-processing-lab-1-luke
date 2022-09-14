d21b83418ad8c9d4ca61c281476b0661bcfdbe89
burde041 <burde041@morris.umn.edu>
2022-09-14 17:31:36 -0500

    Fix process_client_logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #Moves to specified directory
-#cd "$1"/var/log
+cd "$1"
 
-#Extracts
-cat *.txt > allfiles.txt
-awk 'match($0, /([a-zA-Z]+\s[0-9]+\s[0-9]+).*(invalid\suser|Failed\spassword\sfor)\s([a-zA-Z]+)\sfrom ([0-9.]+)/, groups) {print groups[1] " " groups[3] " " groups[4]}' < allfiles.txt >> failed_login_data.txt
+#Filters log files for failed login attempts
+cat var/log/secure* | awk 'match($0, /([a-zA-Z]+)\s+([0-9]+\s[0-9]+).*(invalid\suser|Failed\spassword\sfor)\s([a-zA-Z0-9]+)\sfrom\s([0-9\.]+)/, groups) {print groups[1] " " groups[2] " " groups[4] " " groups[5]}' > failed_login_data.txt
