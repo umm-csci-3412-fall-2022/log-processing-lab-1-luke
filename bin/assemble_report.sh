472d03229815e6f69f3814f346910f64a4494633
burde041 <burde041@morris.umn.edu>
2022-09-14 17:41:15 -0500

    Add assemble_report.sh

---
+++ bin/assemble_report.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Moves to given directory
+cd "$1" || return
+
+#Combines the three created html files to generate the report
+cat country_dist.html hours_dist.html username_dist.html > report.html
+
+#Returns to the original directory
+cd "$OLDPWD" || return
+
+#Wratps the report with the relevant header and footer
+./bin/wrap_contents.sh "$OLDPWD"/report.html html_components/summary_plots "$OLDPWD"/failed_login_summary.html
+
+#Removes temporary files
+rm "$OLDPWD/report.html"
